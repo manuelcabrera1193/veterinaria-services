a0728b0e608094c4e7f4d1fdcc32038236f01d42
manuelcabrera1193 <72555518+manuelcabrera1193@users.noreply.github.com>
2023-03-27 00:27:21 -0500

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
     host: "localhost",
     database: "veterinaria",
-    user: "root",
+    user: "admin",
     password: "admin",
 })
 
